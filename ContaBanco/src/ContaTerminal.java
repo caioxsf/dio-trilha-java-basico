33f9d293f495f8ebc9d6a6eddc2f3cfdc0ba7ce8
caioxsf <caio.nobile@hotmail.com>
2024-04-25 14:09:57 -0300

    testando

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,9 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String nome = "Caio";
+
+        System.out.println(nome);
     }
 }
